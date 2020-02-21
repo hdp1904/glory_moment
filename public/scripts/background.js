6dca4cf1af52ce52af9f46cd87f551776912d46d
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2020-02-20 16:15:40 -0800

    Update background.js

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,7 +1,6 @@
 var counter = 0;
 function changeBG(){
     var imgs = [
-        //
         "url(https://i.imgur.com/WIdaEZv.jpg)",
         "url(https://i.imgur.com/WcFVNhz.jpg)",
         "url(https://i.imgur.com/lDQMIDZ.jpg)",
@@ -14,7 +13,6 @@ function changeBG(){
     
     if(counter === imgs.length) counter = 0;
     $("body").css("background-image", imgs[counter]);
-//
     counter++;
 }
   
