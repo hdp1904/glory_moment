08bea863ef3be0155415a2671f65c6e51e4e4797
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2024-09-18 17:58:30 -0700

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -8,7 +8,7 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
     console.log(req.params.id);
     Campground.findById(req.params.id, function(err, campground){
         if(err){
-            console.error(err);
+            console.log(err);
         } else {
              res.render("comments/new", {campground: campground});
         }
