adfcf0920b87119dd1bc7d1e74014b5183ef048b
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2024-10-19 09:20:03 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String, 
+ 
     password: String,
     avatar: String,
     firstName: String,
