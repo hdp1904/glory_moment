202d9b140f4b2c2eb7bdae5b26a4ceb6e34df3ca
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-02-17 01:03:05 -0800

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,7 +5,7 @@ var middleware = require("../middleware");
 var NodeGeocoder = require('node-geocoder');
  
 var options = {
-  provider: 'google',
+
   httpAdapter: 'https',
   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null
