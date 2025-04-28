0f1e01c85d282f32ef9a6b2e5d56495136d54798
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-04-28 09:46:34 -0700

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router  = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
-var Comment = require("../models/comment");
+
 var middleware = require("../middleware");
 
 router.get("/new", middleware.isLoggedIn, function(req, res){
