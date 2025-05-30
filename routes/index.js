2053ae31fcb668358c12c151d158e35444d3296d
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-05-30 00:27:14 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var User = require("../models/user");
 var Campground = require("../models/campground");
 var async = require("async");
 var nodemailer = require("nodemailer");
-
+var crypto = require("crypto");
 
 router.get("/", function(req, res){
     res.render("landing"); 
