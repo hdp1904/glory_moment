b07ef64661afa5317e84115699e12524ef3357a8
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-11-22 06:12:21 -0800

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,7 @@ var campgroundSchema = new mongoose.Schema({
    // wait to change
    name: String,
    image1: String,
-   image2: String,
+
    image3: String,
    image4: String,
    image5: String,
