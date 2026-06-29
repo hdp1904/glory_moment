226176e40426a70c03b32b869009667acfc1665f
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-06-29 10:26:07 -0700

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -1,1 +1,33 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+   // wait to change
+   name: String,
+   image1: String,
+   image2: String,
+   image3: String,
+   image4: String,
+   image5: String,
+   description: String,
+   cost: { type: String, required: true },
+   location: String,
+   lat: Number,
+   lng: Number,
+   createdAt: { type: Date, default: Date.now },
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
 
