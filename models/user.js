6947ccfcfc487332f1dd8e49aa8d84f360fdd07c
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2026-08-03 09:57:22 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,18 +1,1 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
 
-var UserSchema = new mongoose.Schema({
-    username: String, 
-    password: String,
-    avatar: String,
-    firstName: String,
-    lastName: String,
-    email: {type: String, unique: false},
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-    isAdmin: {type: Boolean, default: false}
-});
-
-UserSchema.plugin(passportLocalMongoose)
-
-module.exports = mongoose.model("User", UserSchema);
